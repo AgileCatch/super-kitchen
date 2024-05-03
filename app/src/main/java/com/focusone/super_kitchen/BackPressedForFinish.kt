1b5afffa7b335229511e4c87a044c2690fc12ae2
김영현 <agilecatch@gmail.com>
2024-05-03 13:22:45 +0900

    [fix] : 뒤로가기 버튼 주석 추가

--- app/src/main/java/com/focusone/super_kitchen/BackPressedForFinish.kt
+++ app/src/main/java/com/focusone/super_kitchen/BackPressedForFinish.kt
@@ -9,10 +9,20 @@ class BackPressedForFinish(private val activity: Activity) {
     private var toast: Toast? = null
 
     fun onBackPressed() {
+        // '뒤로' 버튼 클릭 시간과 현재 시간을 비교 게산한다.
+
+        // 마지막 '뒤로'버튼 클릭 시간이 이전 '뒤로'버튼 클릭시간과의 차이가 TIME_INTERVAL(여기서는 2초)보다 클 때 true
         if (System.currentTimeMillis() > backKeyPressedTime + TIME_INTERVAL) {
+
+            // 현재 시간을 backKeyPressedTime에 저장한다.
             backKeyPressedTime = System.currentTimeMillis()
+
+            // 종료 안내문구를 노출한다.
             showMessage()
         } else {
+            // 마지막 '뒤로'버튼 클릭시간이 이전 '뒤로'버튼 클릭시간과의 차이가 TIME_INTERVAL(2초)보다 작을때
+
+            // Toast가 아직 노출중이라면 취소한다.
             toast?.cancel()
             activity.finish()
             System.exit(0)
