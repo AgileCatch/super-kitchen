36bdabadad4ad693357ff6e986c064be329bb736
김영현 <agilecatch@gmail.com>
2024-05-07 14:53:54 +0900

    [add] : Helper추가

---
+++ app/src/main/java/com/focusone/super_kitchen/util/FileChooserHelper.kt
@@ -0,0 +1,21 @@
+package com.focusone.super_kitchen.util
+
+import android.content.Context
+import android.content.Intent
+
+class FileChooserHelper(private val context: Context) {
+    fun show() {
+        val chooserIntent = createChooserIntent()
+        context.startActivity(chooserIntent)
+    }
+
+    private fun createChooserIntent(): Intent {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "*/*"
+            val mimeTypes = arrayOf("image/*", "application/pdf")
+            putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+        }
+        return Intent.createChooser(intent, "첨부파일 선택")
+    }
+}
